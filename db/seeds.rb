be856cf26fbec8c48b5334a7ad6909502c868896
vulcanobr <vulcanorjbr@gmail.com>
2023-02-10 12:18:59 -0300

    Db Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,11 @@
 # Tabela de Usuario
 def Usuario
     User.create!(name: "Vulcano", email: "vulcano@email.com", password: 123456)
+    User.create!(name: "Sanurai", email: "sanurai@email.com", password: 123456)
 end
 
 # Tabela de Categoria
-def categoria
+def Categoria
     cat = ["Carro", "2 Rodas", "Caminhão Toco", "Furgão", "VUC", "Van", "Truck"]
     7.times do |index|
       Category.create!(
@@ -23,5 +24,138 @@ def categoria
 end
 
 #Tabela de Veiculos
+def Car 
+    fabricante = ["Suzuki", "Fiat", "Ford", "Chevrolet", "Honda", "Hunday", "Citroen", "Renault"]
+    p = Category.select(:id).shuffle[0..6]
+    v = Vehicle.select(:id).shuffle[0..19]
+    50.times do |index|
+      Vehicle.create!(
+            nameplate: SecureRandom.alphanumeric(8).upcase, 
+            brand: fabricante[rand(0..7)],
+            vehicle_model: "Modelo  #{index}",
+            year_manufacture: rand(2015..2023), 
+            maximum_load: rand(10..14000),
+            status: 0,
+            category_id: p[rand(0..6)].id)
+    end
+end
+  
+# Tabela de Modalidade, Preço e Prazo
+def Modalidade
+    mod = ["Carga Rapida", "Carga Expressa", "Carga Pesada", "Caraga Interestadual", "Carga Intermunicipal", "Leva e Traz",
+            "Carga Leve" ] 
 
-# Tabela de Modalidade
+    7.times do |index|
+        ModeTransport.create!(
+            name: mod[index],
+            minimum_distance: 0,
+            maximum_distance: rand(50..1300),
+            minimum_weight: 0,
+            maximum_weight: rand(10..100),
+            delivery_fee: rand(30..200),
+            status: 0
+        )
+    end
+
+        mt = ModeTransport.all
+        mt.each do |ind|
+            startkg = 0 
+            finalkg = 18
+            km = 50
+            6.times do |indx|
+                Price.create!(
+                    start_weight: startkg,
+                    final_weight: finalkg,
+                    km_price: km,
+                    mode_transport_id: ind.id)
+                
+                startkg = finalkg + 1
+                finalkg += 18
+                km += 70
+            end
+        end
+        mt1 = ModeTransport.all
+        
+        mt1.each do |i|
+            startkm = 0
+            finalkm = 100
+            hs = 24
+            6.times do |indx|
+                Deadline.create!(
+                    start_distance: startkm,
+                    final_distance: finalkm,
+                    deadline_hours: hs,
+                    mode_transport_id: i.id)
+                
+                startkm = finalkm + 1
+                indx > 3 ? finalkm += 350 : finalkm += 200
+                indx > 3 ? hs += 48 : hs += 24
+                
+            end
+        end
+end
+
+# Tabela Ordem de Entrada
+def Order 
+    user = User.select(:id).shuffle[0..1]
+    end1 = ["Rua", "Avenida", "Estrada", "Travessa", "Praça"]
+    end2 = ["São Cristovão", "Samuel Rosa", "Santa Clara", "Treze de Março", "XV de Julho", "São Salvador", "Maria Cristina",
+            "Treze de Maio", "Bolçao Azul", "Hilda Magalhâes"]
+    
+    end3 = ["Freguesia", "Jabaguara", "Macarena", "Matheuzinho", "Silva Jardim", "Tatuape", "Taquara", "Ilha Raza", 
+            "Noronha", "Cascadura"]
+    end4 = ["Campina Grande", "Santos", "Maringa", "Dourados", "Macapa", "Nova Aurora", "Cruz Azul", "Ribeirão Preto", 
+            "Alagoinhas", "Vila Azul"]
+    end5 = ["MT", "RD", "MA", "AP", "SC", "GO", "PB", "RN", "PE", "SP"]
+    sender = ["Carlos Magno", "Josival Santos", "Matia Cristina", "Juliana Silva", "Fernando Sanches", 
+                  "Marcio Nogueira", "Kamila Campelo", "Tainara Terica", "Ramom Dias", "Valeria Kurusawa"]
+    recepient = ["Mendes Alves", "Mirian Novaes", "Julia Mesquita", "Carla Cristina", "Albuquerque Silva",
+                   "Marlon Dias", "Deyse Samuel", "Clara Marcondes", "Sibila Amaral", "Jackson Mendes"]
+    20.times do |index|
+        Order.create!(
+            code: SecureRandom.alphanumeric(15).upcase,
+            product_code: SecureRandom.alphanumeric(8).upcase,
+            height: rand(10..100),
+            width: rand(30..150),
+            depth: rand(5..50),
+            weight: rand(8..100),
+            description: "descrição padrão #{index + 1}",
+            distance: rand(80..1400),
+            sender_name: sender[rand(0..9)],
+            sender_identification: rand(11111111111..99999999999),
+            sender_email: "#{SecureRandom.alphanumeric(6)}@email.com",
+            sender_phone: "(#{rand(11..88)}) #{rand(11111..99999)}-#{rand(1111..9999)}",
+            sender_address: "#{end1[rand(0..4)]} #{end2[rand(0..9)]}, #{rand(23..400)}, #{end3[rand(0..9)]}",
+            sender_city: end4[rand(0..9)],
+            sender_state: end5[rand(0..9)],
+            sender_zipcode: "#{rand(11111..99999)}-#{rand(111..999)}",
+            recipient_name: recepient[rand(0..9)],
+            recipient_identification: rand(11111111111..99999999999),
+            recipient_email: "#{SecureRandom.alphanumeric(6)}@email.com",
+            recipient_phone: "(#{rand(11..88)}) #{rand(11111..99999)}-#{rand(1111..9999)}", 
+            recipient_address: "#{end1[rand(0..4)]} #{end2[rand(0..9)]}, #{rand(23..400)}, #{end3[rand(0..9)]}",
+            recipient_city: end4[rand(0..9)],
+            recipient_state: end5[rand(0..9)],
+            recipient_zipcode: "#{rand(11111..99999)}-#{rand(111..999)}",
+            status: 0,
+            user_id: user[rand(0..1)].id)
+    end
+end
+
+Order.destroy_all
+User.destroy_all
+Usuario()
+
+#Category.destroy_all
+#Categoria()
+
+#Vehicle.destroy_all
+#Car()
+
+Deadline.destroy_all
+Price.destroy_all
+ModeTransport.destroy_all
+Modalidade()
+
+
+Order()
--- spec/system/category/user_delete_category_spec.rb
+++ spec/system/category/user_delete_category_spec.rb
@@ -5,7 +5,8 @@ describe 'Usuario remove Categoria' do
     it 'com sucesso' do
         
         # Arrange
-        category = Category.create!(name: 'Carro')
+        category1 = Category.create!(name: "Carro")
+        category2 = Category.create!(name: "Moto")
         user = User.create!(name: 'Vulcano', email: 'vulcano@email.com', password: 'password' )
         
         # Act
@@ -13,7 +14,9 @@ describe 'Usuario remove Categoria' do
         visit root_path
         click_on 'Cadastros'
         click_on 'Categorias'
-        click_on 'Remover'
+        within('td', id: 'category_1') do
+            click_on 'Remover'
+        end
 
         # Assert
         expect(current_path).to eq categories_path
